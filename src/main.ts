330a0f8dd89a491e5cee547c51529e750b68c056
default <61233505+dev-cetus@users.noreply.github.com>
2024-06-21 22:08:52 +0000

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import './style.css'
+
+// @ts-ignore
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+<div class="bg-slate-950 flex items-center justify-center h-screen w-screen">
+    <img src="https://s3.splrge.dev/assets/brand/labs/banner_text_white.png" alt="SPLRGE Banner" class="animate-fade-in z-50" />
+</div>
+<div class="spotlight"></div>
+
+`
